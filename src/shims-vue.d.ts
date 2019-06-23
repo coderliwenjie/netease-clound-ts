4234d1871991167456493169dfd8b8747c1026bf
coderliwenjie <943207011@qq.com>
2019-06-23 22:59:44 +0800

    歌词解析

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,9 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+declare module "utils/lyric-parse" {
+  export class Lyric {
+    public _init(): void
+    constructor(lrc: string, hanlder: object)
+  }
+}
--- src/views/song/Index.vue
+++ src/views/song/Index.vue
@@ -1,16 +1,16 @@
 <template>
-  
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import api from '../../api'
-import LyricParse from '../../utils/lyric-parse.js'
+import {Lyric} from 'utils/lyric-parse'
+/// <reference path="../../../shims-vue.d.ts" />
 @Component
 export default class Song extends Vue {
   private async created() {
     api.songDetail(this.$route.query.id as string)
     const lyric = await api.lyric(this.$route.query.id as string)
-    console.log(new LyricParse(lyric.lrc.lyric))
+    console.log(new Lyric(lyric.lrc.lyric, () => { console.log('ss') }))
   }
 }
 </script>
